2bff4b131ef6abf5ef1d2b515fb4a97e202fdc5b
MKS-students <mks.students@hackreactor.com>
2016-11-08 10:57:27 -0600

    Passing SongQueue tests.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,28 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    // listens for the dequeue event and removes the song when it is dequeued
+    this.on('dequeue', function() {
+      this.remove(this.at(0));
+    }),
+    // listens for the add event for the collection
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }),
+    // lists for the ended event for the collection
+    this.on('ended', function() {
+      this.at(0).dequeue();
+      if(this.length >= 1) {
+        this.playFirst();
+      }
+    })
+
+  }, //<-- end initialize
+
+  playFirst: function () {
+    this.at(0).play();
   }
 
 });
